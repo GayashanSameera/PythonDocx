6ede8116d6074ef38d003689f0a3ef87019b9739
GayashanSameera <gayashansameera30@gmail.com>
2022-07-05 08:48:42 +0530

    add data

--- template_update.py
+++ template_update.py
@@ -93,7 +93,10 @@ if __name__ == '__main__':
             'var12': "3,000",
             'var13':"7,000",
             'spart': "sample text",
-            'scustomer': "sample custome"
+            'scustomer': "sample customer",
+            'sname': "test",
+            'suser': "gayashan",
+            'execSum': "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature  from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word "
             },
         "table_replaces": {
             "cashFlows":{
